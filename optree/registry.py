a9628dd52b2252975c9e58fd0ee3438007b2695d
Xuehai Pan <XuehaiPan@pku.edu.cn>
2025-02-23 00:49:22 +0800

    test

--- optree/registry.py
+++ optree/registry.py
@@ -165,7 +165,7 @@ def pytree_node_registry_get(
 ) -> dict[type, PyTreeNodeRegistryEntry]: ...
 
 
-# pylint: disable-next=too-many-return-statements,too-many-branches
+# pylint: disable-next=too-many-return-statements
 def pytree_node_registry_get(  # noqa: C901
     cls: type | None = None,
     /,
@@ -228,22 +228,15 @@ def pytree_node_registry_get(  # noqa: C901
     """
     if namespace is __GLOBAL_NAMESPACE:
         namespace = ''
-    if (
-        cls is not None
-        and cls is not namedtuple  # noqa: PYI024
-        and not inspect.isclass(cls)
-    ):
-        raise TypeError(f'Expected a class or None, got {cls!r}.')
-    if not isinstance(namespace, str):
-        raise TypeError(f'The namespace must be a string, got {namespace!r}.')
 
     if cls is None:
         namespaces = frozenset({namespace, ''})
-        registry = {
-            handler.type: handler
-            for handler in _NODETYPE_REGISTRY.values()
-            if handler.namespace in namespaces
-        }
+        with __REGISTRY_LOCK:
+            registry = {
+                handler.type: handler
+                for handler in _NODETYPE_REGISTRY.values()
+                if handler.namespace in namespaces
+            }
         if _C.is_dict_insertion_ordered(namespace):
             registry[dict] = _DICT_INSERTION_ORDERED_REGISTRY_ENTRY
             registry[defaultdict] = _DEFAULTDICT_INSERTION_ORDERED_REGISTRY_ENTRY
